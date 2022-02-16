e8a0bd3c0d8bb9e46f1583c441105b57302d76d4
gjacobu <gjacobus1@cub.uca.edu>
2022-02-15 20:01:57 -0700

    finishing up part 1a

--- part1/fig1a/logistic_map.py
+++ part1/fig1a/logistic_map.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+
+'''
+This code is based on the code found here:
+https://colab.research.google.com/github/wdjpng/chaos/blob/main/main.ipynb
+'''
+
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 import numpy as np
 
@@ -11,17 +18,29 @@ x = np.arange(0, resolution,1).tolist()
 y = [None] * resolution
 k = 1
     
-def iterative(r):
-    y[0]=0.2
+def iterative(r, y0):
+    y[0]=y0
     for t in range(1, resolution):
         y[t]=r*y[t-1]*(1.0-y[t-1]/k)
         
-def plot_func(r):
-    iterative(r)
+def plot_func(r, y0, label=""):
+    iterative(r, y0)
     plt.xlabel("t")
     plt.ylabel("Population size")
-    plt.plot(x, y)
+    plt.plot(x, y, label=label)
 
-plot_func(3)
-plot_func(3.2)
-plt.show()
+r = 2.8
+plot_func(r, 0.2, f"r={r}, y0=0.2")
+plot_func(r,0.25, f"r={r}, y0=0.25")
+plt.rcParams["figure.figsize"] = (16,12)
+plt.legend(loc="upper right")
+plt.savefig('fig_1a_1.png', dpi=1000)
+
+plt.clf()
+
+r = 3.9
+plot_func(r, 0.2, f"r={r}, y0=0.2")
+plot_func(r, 0.3, f"r={r}, y0=0.3")
+plt.rcParams["figure.figsize"] = (16,12)
+plt.legend(loc="upper right")
+plt.savefig('fig_1a_2.png', dpi=1000)
